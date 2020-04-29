e43060f1e570d6c6672128856c20006ba6aae055
ren(蓮) <63449550+ren778@users.noreply.github.com>
2020-04-30 08:36:51 +0900

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function URLCopy() {
+  navigator.clipboard.writeText("https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js");
+}
+function htmlCopy() {
+  navigator.clipboard.writeText('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>');
+}
